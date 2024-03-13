42ed46b83649c06b38630b912efb3612373e484d
roushan-sheik <roushansheik@gmail.com>
2024-03-13 10:10:38 +0600

    profile and banner image added

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,8 +1,54 @@
 import React from "react";
-
+import { CiCalendarDate } from "react-icons/ci";
+import { FaRegBookmark } from "react-icons/fa6";
 const Blog = ({ blog }) => {
-  const { id, cover, title, author_img, date, reading_time, hashtags } = blog;
-  return <div>Blog</div>;
+  const { id, cover, title, name, author_img, date, reading_time, hashtags } =
+    blog;
+  return (
+    <div>
+      {/* image box  */}
+      <div className="h-[25rem] w-full object-cover rounded-md">
+        <img className="w-full rounded-md h-full" src={cover} alt="Blog Img" />
+      </div>
+      {/* profile box  */}
+      <div className="flex justify-between mt-6 items-center">
+        {/* left box  */}
+        <div className="flex gap-5 items-center">
+          <div className="w-[50px] h-[50px] rounded-full object-cover">
+            <img
+              className="w-full h-full rounded-full"
+              src={author_img}
+              alt="Author"
+            />
+          </div>
+          <div>
+            <h4 className="font-bold text-2xl primary cursor-pointer">
+              {name}
+            </h4>
+            <div className="flex text-base gap-2 items-center">
+              <CiCalendarDate />
+              <p className="secondary text-sm">{date}</p>
+            </div>
+          </div>
+        </div>
+        {/* right box  */}
+        <div className="flex gap-2 text-lg items-center secondary">
+          <p>{`${reading_time} min read`}</p>
+          <button className="cursor-pointer">
+            <FaRegBookmark />
+          </button>
+        </div>
+      </div>
+      {/* content box  */}
+      <div>
+        {/* title */}
+        <h2></h2>
+        {/* hash tag  */}
+        {/* button  */}
+        <button>Mark as read</button>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="w-[80%] mx-auto">
+    <div className="w-[80%] mx-auto max-w-[1440px]">
       {/* header  */}
       <Header />
       <main className="flex flex-col lg:flex-row">{children}</main>
